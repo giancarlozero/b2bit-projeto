3ceec14f5f507f679d53a694887a46d719718a0c
Giancarlo Silva <giancarlozero@gmail.com>
2025-09-28 22:42:41 -0300

    Correção.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       }
 
       router.push("/profile");
-    } catch (error) {
+    } catch (error: any) {
       console.error("Erro de login:", error);
 
       if(error.response?.status === 401) {
